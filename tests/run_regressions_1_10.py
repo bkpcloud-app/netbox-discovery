d6873f1d0c9f1a8448b4c620da033fd3442957ca
bkpcloud-app <diegodmanso@gmail.com>
2026-08-04 13:03:44 -0300

    1.11.8: update release regression version

--- tests/run_regressions_1_10.py
+++ tests/run_regressions_1_10.py
@@ -15,8 +15,8 @@ from modules.product import updater
 def test_release_version():
     root_version = open(os.path.join(ROOT, "VERSION"), "r").read().strip()
     package_version = open(os.path.join(BASE, "VERSION"), "r").read().strip()
-    assert root_version == package_version == "1.11.7"
-    assert updater.version_key("1.11.7") > updater.version_key("1.11.6")
+    assert root_version == package_version == "1.11.8"
+    assert updater.version_key("1.11.8") > updater.version_key("1.11.7")
 
 
 def main():
